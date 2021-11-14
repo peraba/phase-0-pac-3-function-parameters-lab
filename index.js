1b8f07d2350522d889d449bf7902253083c590e5
Pedro Ramos <medpsr9@gmail.com>
2021-11-14 16:53:18 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function sayHelloTo(firstName = "User") {
+    console.log(`Hello, ${firstName}!`);
+  }
+
+  function introductionWithLanguageOptional(language = "JavaScrpt") {
+    console.log(`Hello, ${firstName}!`);
+  }
+
+  function sayHelloTo(name = "User") {
+    console.log(`Hello, ${name}!`);
+  }
